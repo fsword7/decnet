f06854c0bae236d3947f918c3e388c47a31ca073
patrick <>
2001-02-12 15:27:25 +0000

    Debian package now builds lintian clean. More testing needed tho'

--- latd/debian/init.d
+++ latd/debian/init.d
@@ -1,22 +1,6 @@
 #!/bin/sh
 #
-# startlat.sh
-#
 # Starts/Stops latd process
-#
-# This script should go in /etc/init.d      (Debian)
-#                          /etc/rc.d/init.d (RedHat)
-#                          /sbin/init.d     (SuSE)
-#
-# and you should link to it from the relevant runlevel startup directory
-# eg: (Debian)
-#      ln -s /etc/init.d/startlat.sh /etc/rc2.d/S79lat.sh
-#
-#     (RedHat)
-#      ln -s /etc/rc.d/init.d/startlat.sh /etc/rc.d/rc3.d/S79lat
-#
-#     (SuSE)
-#      ln -s /sbin/init.d/startlat.sh /sbin/init.d/rc2.d/S79lat
 #
 # -----------------------------------------------------------------------------
 #
@@ -45,13 +29,10 @@ fi
 #
 # Look for latcp
 #
-LATCP=""
-[ -x /usr/sbin/latcp ] && LATCP="/usr/sbin/latcp"
-[ -x /usr/local/sbin/latcp ] && LATCP="/usr/local/sbin/latcp"
+LATCP="/usr/sbin/latcp"
 
-if [ -z "$LATCP" ]
+if [ ! -f "$LATCP" ]
 then
-  echo "Cannot find latcp."
   exit 1
 fi
 
@@ -60,27 +41,26 @@ case $1 in
      echo -n "Starting LAT: "
      $LATCP -s
      STATUS=$?
-     echo "$startendecho"
+     echo "latd."
      ;;
 
    stop)
      echo -n "Stopping LAT... "
      $LATCP -h
      STATUS=$?
-     echo "$stopendecho"
+     echo "done."
      ;;
 
-   restart|reload)
+   restart|reload|force-reload)
      echo -n "Restarting LAT: "
      $LATCP -h
      $LATCP -s
      STATUS=$?
-     echo -n "$startecho"
-     echo "$stopendecho"
+     echo "done."
      ;;
 
    *)
-     echo "Usage $0 {start|stop|restart}"
+     echo "Usage $0 {start|stop|restart|force-reload}"
      STATUS=0;
      ;;
 esac

7824a8015438da66315998023961c9bf69ec6059
patrick <>
2001-11-30 13:57:54 +0000

    You'll need this too.

--- latd/moprc.h
+++ latd/moprc.h
@@ -4,6 +4,7 @@
 #define MOPRC_CMD_RESERVE      0x0D
 #define MOPRC_CMD_RELEASE      0x0F
 #define MOPRC_CMD_REQUESTID    0x05
+#define MOPRC_CMD_BOOT         0x06
 #define MOPRC_CMD_SYSTEMID     0x07
 #define MOPRC_CMD_COMMANDPOLL  0x11
 #define MOPRC_CMD_RESPONSE     0x13

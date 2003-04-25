f62b0595cf14448a3ce2ea73751e631339a3e6aa
patrick <>
2003-04-25 07:19:22 +0000

    Don't issue that stupid "no patched kernel in /usr/src" message any more. If we cant find a kernel assume it's 2.4 or higher.

--- dnprogs/scripts/check_kernel.sh
+++ dnprogs/scripts/check_kernel.sh
@@ -43,21 +43,7 @@ then
   #
   # Use our fallback include file
   #
+  echo Assuming 2.4+ kernel
   cp include/kernel/netdnet/dn.h include/netdnet
-  echo '*********************************************************************'
-  echo I can\'t find a patched kernel in /usr/src.
-  echo
-  echo If you haven\'t patched your kernel yet then I recommend you do
-  echo so before compiling these programs because they certainly won\'t
-  echo work without DECnet support in the kernel and it is important
-  echo that the programs know which version of the kernel patch you
-  echo are using.
-  echo
-  echo You can still compile the programs without the kernel available by
-  echo typing the command \'make please\' but be aware that some of the
-  echo programs may not work correctly or at all. If you do this so make 
-  echo sure you know what you are doing.
-  echo
-  return 1
 fi
 return 0

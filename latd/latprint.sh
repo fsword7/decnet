edad46aaa970809d00da8381c350ba1db503e961
patrick <>
2005-03-06 13:38:13 +0000

    Include nodename in VMS command

--- latd/latprint.sh
+++ latd/latprint.sh
@@ -2,14 +2,15 @@
 #
 # latprint.sh
 #
-# This is an example script for using latd as a LAT printer server
+# This is an example script for using latd as a LAT printer server.
+# Use the commands below substituting the bits in CAPS for your own.
 #
 # on Linux:
 #   latcp -A -a PRINTER -C '/bin/sh /usr/local/sbin/latprint.sh' -u root
 #
 # on VMS:
 #   mcr latcp create port lta999
-#   mcr latcp set port lta999/noqueue/service=PRINTER
+#   mcr latcp set port lta999/noqueue/service=PRINTER/node=LINUX
 #   init/queue/process=latsym/on=lta999:/start LINUXLAT
 #
 

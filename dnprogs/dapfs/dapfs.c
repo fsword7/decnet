e63dbd9028f974823aabd1ab9aacefc527f068d7
patrick <>
2005-10-16 10:46:28 +0000

    A bit more

--- dnprogs/dapfs/dapfs.c
+++ dnprogs/dapfs/dapfs.c
@@ -10,14 +10,10 @@
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
- ******************************************************************************
- */
+    ******************************************************************************
+    */
 /* dapfs via FUSE */
-
-/*
-    This program can be distributed under the terms of the GNU GPL.
-    See the file COPYING.
-*/
+//  mount -tfuse  dapfs#'alpha1"patrick password"::' /mnt/dap
 
 #define _FILE_OFFSET_BITS 64
 #define FUSE_USE_VERSION 22
@@ -37,13 +33,87 @@
 #include "dapfs_dap.h"
 #include "filenames.h"
 
+/* Copied from dncopy */
+
 struct dapfs_handle
 {
 	RMSHANDLE rmsh;
+	/* RMS File attributes */
+	int org;
+	int rat;
+	int rfm;
+	int mrs;
+	int fsz;
+	/* Last known offset in the file */
 	off_t offset;
 };
 
 char prefix[1024];
+
+static const int RAT_DEFAULT = -1; // Use RMS defaults
+static const int RAT_FTN  = 1; // RMS RAT values from fab.h
+static const int RAT_CR   = 2;
+static const int RAT_PRN  = 4;
+static const int RAT_NONE = 0;
+
+static const int RFM_DEFAULT = -1; // Use RMS defaults
+static const int RFM_UDF = 0; // RMS RFM values from fab.h
+static const int RFM_FIX = 1;
+static const int RFM_VAR = 2;
+static const int RFM_VFC = 3;
+static const int RFM_STM = 4;
+static const int RFM_STMLF = 5;
+static const int RFM_STMCR = 6;
+
+
+/* Convert RMS record carriage control into something more unixy */
+static int convert_rms_record(char *buf, int len, struct dapfs_handle *fh)
+{
+	int retlen = len;
+
+	/* If the file has implied carriage control then add an LF to the end of the line */
+	if ((fh->rfm != RFM_STMLF) &&
+	    (fh->rat & RAT_CR || fh->rat & RAT_PRN))
+		buf[retlen++] = '\n';
+
+	/* Print files have a two-byte header indicating the line length. */
+	if (fh->rat & RAT_PRN)
+	{
+		memmove(buf, buf + fh->fsz, retlen - fh->fsz);
+		retlen -= fh->fsz;
+	}
+
+/* FORTRAN files have a leading character that indicates carriage control */
+	if (fh->rat & RAT_FTN)
+	{
+		switch (buf[0])
+		{
+
+		case '+': // No new line
+			buf[0] = '\r';
+			break;
+
+		case '1': // Form Feed
+			buf[0] = '\f';
+			break;
+
+		case '0': // Two new lines
+			memmove(buf+1, buf, retlen+1);
+			buf[0] = '\n';
+			buf[1] = '\n';
+			retlen++;
+			break;
+
+
+		case ' ': // new line
+		default:  // Default to a new line. This seems to be what VMS does.
+			buf[0] = '\n';
+			break;
+		}
+	}
+
+	return retlen;
+}
 
 static int dapfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
@@ -80,24 +150,60 @@ int dapfs_chmod(const char *path, mode_t mode)
 	return 0;
 }
 
+int dapfs_chown(const char *path, uid_t u, gid_t g)
+{
+	// Can we do this ??
+	return 0;
+}
+
+static int dapfs_flush(const char *path, struct fuse_file_info *fi)
+{
+	// NO-OP
+	return 0;
+}
+
 static int dapfs_truncate(const char *path, off_t size)
 {
-	// TODO - DAP::CONTROL::TRUNCATE message.
-	// NOTE: This can also be called at create time to overwrite an
-	// existing file
-	return  0;
+	RMSHANDLE rmsh;
+	int offset;
+	int res;
+	struct RAB rab;
+	char fullname[2048];
+	char vmsname[2048];
+
+	make_vms_filespec(path, vmsname, 0);
+	sprintf(fullname, "%s%s", prefix, vmsname);
+
+	rmsh = rms_open(fullname, O_WRONLY, NULL);
+	if (!rmsh) {
+		return -errno;
+	}
+
+	memset(&rab, 0, sizeof(rab));
+	if (size) {
+		rab.rab$l_kbf = &offset;
+		rab.rab$b_rac = 2;//FB$RFA;
+		rab.rab$b_ksz = sizeof(offset);
+	}
+	res = rms_find(rmsh, &rab);
+	if (!res)
+		goto finish;
+	res = rms_truncate(rmsh, NULL);
+finish:
+	rms_close(rmsh);
+	return res;
 }
 
 static int dapfs_mkdir(const char *path, mode_t mode)
 {
-	syslog(1, "PJC: dapfs_mkdir called for %s\n", path);
+	syslog(LOG_DEBUG, "PJC: dapfs_mkdir called for %s\n", path);
 	// TODO Use remote object
 	return -ENOSYS;
 }
 
 static int dapfs_statfs(const char *path, struct statfs *stbuf)
 {
-	syslog(1, "PJC: dapfs_statfs called for %s\n", path);
+	syslog(LOG_DEBUG, "PJC: dapfs_statfs called for %s\n", path);
 	// TODO Use remote object
 	return -ENOSYS;
 }
@@ -111,18 +217,13 @@ static int dapfs_mknod(const char *path, mode_t mode, dev_t dev)
 	char fullname[2048];
 	char vmsname[2048];
 
-	syslog(1, "PJC: mknod, path=%s, mode=%x\n", path, mode);
-
-	if (S_ISDIR(mode))
-		return -ENOSYS; // Does this happen ??
-
 	if (!S_ISREG(mode))
 		return -ENOSYS;
 
 	make_vms_filespec(path, vmsname, 0);
 	sprintf(fullname, "%s%s", prefix, vmsname);
 
-	rmsh = rms_open(fullname, O_CREAT|O_WRONLY, NULL);
+	rmsh = rms_t_open(fullname, O_CREAT|O_WRONLY, "rfm=stmlf");
 	if (!rmsh)
 		return -errno;
 	rms_close(rmsh);
@@ -131,101 +232,117 @@ static int dapfs_mknod(const char *path, mode_t mode, dev_t dev)
 
 static int dapfs_open(const char *path, struct fuse_file_info *fi)
 {
-    struct dapfs_handle *h;
-    char fullname[2048];
-    char vmsname[2048];
+	struct dapfs_handle *h;
+	struct FAB fab;
+	char fullname[2048];
+	char vmsname[2048];
 
-    h = malloc(sizeof(struct dapfs_handle));
-    if (!h)
-	    return -ENOMEM;
+	syslog(LOG_DEBUG, "open %s, flags=%x\n", path, fi->flags);
+	h = malloc(sizeof(struct dapfs_handle));
+	if (!h)
+		return -ENOMEM;
 
-    make_vms_filespec(path, vmsname, 0);
-    sprintf(fullname, "%s%s", prefix, vmsname);
+	memset(&fab, 0, sizeof(struct FAB));
+	make_vms_filespec(path, vmsname, 0);
+	sprintf(fullname, "%s%s", prefix, vmsname);
+	if (fi->flags & O_CREAT)
+		fab.fab$b_rfm = RFM_STMLF;
 
-    h->rmsh = rms_open(fullname, fi->flags, NULL);
-    if (!h->rmsh) {
-	    int saved_errno = errno;
-	    free(h);
-	    return -saved_errno;
-    }
-    fi->fh = (unsigned long)h;
-    h->offset = 0;
-    return 0;
+	h->rmsh = rms_open(fullname, fi->flags, &fab);
+	if (!h->rmsh) {
+		int saved_errno = errno;
+		free(h);
+		return -saved_errno;
+	}
+	/* Save RMS attributes */
+	h->org = fab.fab$b_org;
+	h->rat = fab.fab$b_rat;
+	h->rfm = fab.fab$b_rfm;
+	h->mrs = fab.fab$w_mrs;
+	h->fsz = fab.fab$b_fsz;
+
+	fi->fh = (unsigned long)h;
+	h->offset = 0;
+	return 0;
 }
 
 static int dapfs_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
 {
-    int res;
-    int got = 0;
-    struct RAB rab;
-    struct dapfs_handle *h = (struct dapfs_handle *)fi->fh;
+	int res;
+	int got = 0;
+	struct RAB rab;
+	struct dapfs_handle *h = (struct dapfs_handle *)fi->fh;
 
-    if (!h) {
-	res = dapfs_open(path, fi);
-	if (!res)
-		return res;
-    }
-
-    memset(&rab, 0, sizeof(rab));
-    if (offset && offset != h->offset) {
-        rab.rab$l_kbf = &offset;
-        rab.rab$b_rac = 2;//FB$RFA;
-        rab.rab$b_ksz = sizeof(offset);
-    }
-
-    // TODO this can be quite slow, because it reads records. Maybe we
-    // should abandon librms and pull blocks down as per dncopy
-    do {
-    	res = rms_read(h->rmsh, buf+got, size-got, &rab);
-	if (res > 0)
-		got += res;
-	if (res < 0 && got) {
-	        // Not enough room for a record but we read something
-	        return got;
+	if (!h) {
+		res = dapfs_open(path, fi);
+		if (!res)
+			return res;
 	}
 
-    } while (res > 0);
-    if (res >= 0)
-	res = got;
+	memset(&rab, 0, sizeof(rab));
+	if (offset && offset != h->offset) {
+		rab.rab$l_kbf = &offset;
+		rab.rab$b_rac = 2;//FB$RFA;
+		rab.rab$b_ksz = sizeof(offset);
+	}
 
-    if (res == -1)
-	    res = -errno;
+	/* This can be quite slow, because it reads records.
+	   However, it's safer this way as we can make sense of sequential files. */
+	do {
+		res = rms_read(h->rmsh, buf+got, size-got, &rab);
+		if (res > 0) {
+			res = convert_rms_record(buf+got, res, h);
+			got += res;
+		}
+		if (res < 0 && got) {
+			// Not enough room for a record but we read something
+			return got;
+		}
 
-    h->offset += res;
-    return res;
+	} while (res > 0);
+	if (res >= 0)
+		res = got;
+
+	if (res == -1)
+		res = -errno;
+
+	h->offset += res;
+	return res;
 }
 
 static int dapfs_write(const char *path, const char *buf, size_t size,
-                     off_t offset, struct fuse_file_info *fi)
+		       off_t offset, struct fuse_file_info *fi)
 {
-    int res;
-    int got = 0;
-    struct RAB rab;
-    struct dapfs_handle *h = (struct dapfs_handle *)fi->fh;
+	int res;
+	struct RAB rab;
+	struct dapfs_handle *h = (struct dapfs_handle *)fi->fh;
 
-    if (!h) {
-        res = dapfs_open(path, fi);
-        if (!res)
-                return res;
-    }
+	if (!h) {
+		res = dapfs_open(path, fi);
+		if (!res)
+			return res;
+	}
 
-    memset(&rab, 0, sizeof(rab));
-    if (offset && offset != h->offset) {
-        rab.rab$l_kbf = &offset;
-        rab.rab$b_rac = 2;//FB$RFA;
-        rab.rab$b_ksz = sizeof(offset);
-    }
+	syslog(LOG_DEBUG, "dapfs_write. offset=%d, (%p) fh->offset=%d\n", (int)offset, h, (int)h->offset);
+	memset(&rab, 0, sizeof(rab));
+	if (offset && offset != h->offset) {
+		rab.rab$l_kbf = &offset;
+		rab.rab$b_rac = 2;//FB$RFA;
+		rab.rab$b_ksz = sizeof(offset);
+	}
 
-    res = rms_write(h->rmsh, (char *)buf+got, size-got, &rab);
-    if (res == -1) {
-	    res = -errno;
-    }
-    else {
-	    h->offset += res;
-	    res = size;
-    }
-    return res;
+	res = rms_write(h->rmsh, (char *)buf, size, &rab);
+	if (res == -1) {
+		syslog(LOG_DEBUG, "rms_write returned %d, errno=%d\n", res, errno);
+		res = -errno;
+	}
+	else {
+		h->offset += size;
+		res = size;
+		syslog(LOG_DEBUG, "rms_write returned 0, offset (%p) now=%d\n", h, (int)h->offset);
+	}
+	return res;
 }
 
 static int dapfs_release(const char *path, struct fuse_file_info *fi)
@@ -238,9 +355,11 @@ static int dapfs_release(const char *path, struct fuse_file_info *fi)
 
 	ret = rms_close(h->rmsh);
 	free(h);
+	fi->fh = 0L;
 
 	return ret;
 }
+
 
 static int dapfs_getattr(const char *path, struct stat *stbuf)
 {
@@ -273,7 +392,9 @@ static struct fuse_operations dapfs_oper = {
 	.rename   = dapfs_rename,
 	.mknod    = dapfs_mknod,
 	.chmod    = dapfs_chmod,
+	.chown    = dapfs_chown,
 	.mkdir    = dapfs_mkdir,
+	.flush    = dapfs_flush,
 	.statfs   = dapfs_statfs,
 	.release  = dapfs_release,
 };
@@ -292,4 +413,3 @@ int main(int argc, char *argv[])
 	return fuse_main(argc-1, argv+1, &dapfs_oper);
 }
 
-// TODO statfs & mkkdir, use DECnet object used by old dapfs!

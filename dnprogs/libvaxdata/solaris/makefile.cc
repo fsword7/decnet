b96e6fcaf5dc2352763a851187424864f0bd17fc
patrick <>
2005-10-25 07:32:13 +0000

    Add in libvaxdata

---
+++ dnprogs/libvaxdata/solaris/makefile.cc
@@ -0,0 +1,11 @@
+#
+# makefile.cc - Make library of functions for reading and writing VAX format
+#               data for Solaris using Sun C (cc).
+#
+# Shell command syntax: make -f makefile.cc [ all | libvaxdata | clean ]
+#
+# -O (standard optimization) -Xc (strict ANSI)
+CC     = cc
+CFLAGS = -O -Xc
+
+include makefile.solaris

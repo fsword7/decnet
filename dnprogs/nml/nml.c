4a9dc5cc37ac73aa35d34df384acafe05e8194f6
chrissie_c <>
2008-09-04 14:34:20 +0000

    More work

--- dnprogs/nml/nml.c
+++ dnprogs/nml/nml.c
@@ -20,10 +20,12 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <errno.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/utsname.h>
 #include <net/if.h>
 #include <netinet/in.h>
 #include <netdnet/dn.h>
@@ -35,10 +37,26 @@
 #define NDA_RTA(r)  ((struct rtattr*)(((char*)(r)) + NLMSG_ALIGN(sizeof(struct ndmsg))))
 #endif
 
+#define IDENT_STRING "DECnet for Linux"
+
+#define NODESTATE_UNKNOWN    -1
+#define NODESTATE_ON          0
+#define NODESTATE_OFF         1
+#define NODESTATE_SHUT        2
+#define NODESTATE_RESTRICTED  3
+#define NODESTATE_REACHABLE   4
+#define NODESTATE_UNREACHABLE 5
 
 static struct rtnl_handle talk_rth;
 static struct rtnl_handle listen_rth;
 static int first_time = 1;
+
+static void makeupper(char *s)
+{
+	int i;
+	for (i=0; i<strlen(s); i++) s[i] = toupper(s[i]);
+}
+
 
 static char *if_index_to_name(int ifindex)
 {
@@ -62,11 +80,12 @@ static char *if_index_to_name(int ifindex)
     return buf;
 }
 
-static int send_node(int sock, struct nodeent *n, int exec, char *device)
+static int send_node(int sock, struct nodeent *n, int exec, char *device, int state)
 {
-	char buf[256];
+	char buf[1024];
 	int ptr = 0;
 
+	makeupper(n->n_name);
 	buf[ptr++] = 1; // Here is your data miss
 	buf[ptr++] = 0;
 	buf[ptr++] = 0; // Status
@@ -76,16 +95,38 @@ static int send_node(int sock, struct nodeent *n, int exec, char *device)
 	buf[ptr++] = strlen(n->n_name) | (exec?0x80:0);
 	memcpy(&buf[ptr], n->n_name, strlen(n->n_name));
 	ptr += strlen(n->n_name);
-	/* device */
+
+	/* Device */
 	if (device) {
-		buf[ptr++] = '6'; // TODO get real value!
-		buf[ptr++] = 3; // CIRCUIT
-		buf[ptr++] = 64;
+		buf[ptr++] = 0x36;  //
+		buf[ptr++] = 0x3;   // CIRCUIT
+		buf[ptr++] = 0x40;  // ASCII text
 		buf[ptr++] = strlen(device);
 		strcpy(&buf[ptr], device);
 		ptr += strlen(device);
 	}
 
+	/* Node State */
+	if (state != NODESTATE_UNKNOWN) {
+		buf[ptr++] = 0;   // Node state
+		buf[ptr++] = 0;
+		buf[ptr++] = 0x81; // Data type of 'state'
+		buf[ptr++] = state;
+	}
+	if (exec) {
+		struct utsname un;
+		char ident[256];
+
+		uname(&un);
+		sprintf(ident, "%s V%s", IDENT_STRING, un.release);
+
+		buf[ptr++] = 0x64;
+		buf[ptr++] = 0;     // Node identification
+		buf[ptr++] = 0x40;  // ASCII text
+		buf[ptr++] = strlen(ident);
+		strcpy(&buf[ptr], ident);
+		ptr += strlen(ident);
+	}
 	return write(sock, buf, ptr);
 }
 
@@ -105,7 +146,7 @@ static int send_exec(int sock)
 	exec_node = getnodebyaddr((char*)exec_addr->a_addr, 2, AF_DECnet);
 	dev = getexecdev();
 
-	return send_node(sock, exec_node, 1, dev);
+	return send_node(sock, exec_node, 1, dev, NODESTATE_ON);
 }
 
 /* Called for each neighbour node in the list */
@@ -115,7 +156,6 @@ static int got_neigh(struct sockaddr_nl *who, struct nlmsghdr *n, void *arg)
 	struct rtattr * tb[NDA_MAX+1];
 	int sock = (int)arg;
 
-	dnetlog(LOG_DEBUG, "got_neigh\n");
 	memset(tb, 0, sizeof(tb));
 	parse_rtattr(tb, NDA_MAX, NDA_RTA(r), n->nlmsg_len - NLMSG_LENGTH(sizeof(*r)));
 
@@ -127,22 +167,26 @@ static int got_neigh(struct sockaddr_nl *who, struct nlmsghdr *n, void *arg)
 		int interface = r->ndm_ifindex;
 		int node = faddr & 0x3ff;
 		int area = faddr >> 10;
-		struct nodeent *ne = getnodebyaddr((char *)&faddr, 2, AF_DECnet);
-		struct nodeent tne;
+		struct nodeent *ne = getnodebyaddr((char *)addr, 2, AF_DECnet);
 
 		if (ne) {
-			send_node(sock, ne, 0, if_index_to_name(interface));
+			send_node(sock, ne, 0, if_index_to_name(interface), NODESTATE_REACHABLE);
 		}
 		else {
-			tne.n_name="";
+			struct nodeent tne;
+			unsigned char tne_addr[2];
+
+			tne.n_name = "";
+			tne.n_addr = tne_addr;
 			tne.n_addr[0] = addr[0];
 			tne.n_addr[1] = addr[1];
-			send_node(sock, &tne, 0, if_index_to_name(interface));
+			send_node(sock, &tne, 0, if_index_to_name(interface), NODESTATE_REACHABLE);
 		}
 	}
 	return 0;
 }
 
+/* SHOW ADJACENT NODES */
 static int send_neighbour_nodes(int sock)
 {
 	if (first_time)
@@ -153,8 +197,6 @@ static int send_neighbour_nodes(int sock)
 			return -1;
 		first_time = 0;
 	}
-
-	send_exec(sock);
 
 	/* Get the list of adjacent nodes */
 	if (rtnl_wilddump_request(&listen_rth, AF_DECnet, RTM_GETNEIGH) < 0) {
@@ -171,6 +213,7 @@ static int send_neighbour_nodes(int sock)
 	return 0;
 }
 
+/* SHOW/LIST KNOWN NODES */
 static int send_perm_nodes(int sock)
 {
 	void *nodelist;
@@ -184,7 +227,7 @@ static int send_perm_nodes(int sock)
 	while (nodename)
 	{
 		struct nodeent *n = getnodebyname(nodename);
-		send_node(sock, n, 0);
+		send_node(sock, n, 0, NULL, NODESTATE_UNKNOWN);
 		nodename = dnet_nextnode(nodelist);
 	}
 	dnet_endnode(nodelist);
@@ -208,16 +251,19 @@ static int read_information(int sock, unsigned char *buf, int length)
 	// option & 0x07: entity type
 
 	// entity: 0=node, 1=line, 2=logging, 3=circuit, 4=module 5=area
-	fprintf(stderr, "CC: option=%d. entity=%d\n", option, entity);
+	dnetlog(LOG_DEBUG, "CC: option=%d. entity=%d\n", option, entity);
 
-	// Send the permanent node information ...
 	switch (option)
 	{
-	case 0: // nodes
+	case 0:  // nodes summary
+	case 16: // nodes char
+	case 32: // nodes state
 		if (entity == 0xff)
 			send_perm_nodes(sock);
 		if (entity == 0xfc)
 			send_neighbour_nodes(sock);
+		if (entity == 0x00)
+			send_exec(sock); // TODO not quite right.
 		break;
 	default:
 		break;
@@ -239,7 +285,7 @@ static void unsupported(int sock)
 	// TODO This text should be of the defined type...
 	strcpy(&buf[3], "Unrecognised command");
 
-	write(sock, buf, strlen(&buf[3])+4);
+	write(sock, buf, strlen(&buf[3])+3);
 }
 
 int process_request(int sock, int verbosity)
